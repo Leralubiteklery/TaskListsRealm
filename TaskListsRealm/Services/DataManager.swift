cfe00aa6b24f35cae7e5b11c4f2a5483ce953083
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-09-11 21:25:01 +0300

    Created DataManager

---
+++ TaskListsRealm/Services/DataManager.swift
@@ -0,0 +1,31 @@
+//
+//  DataManager.swift
+//  TaskListsRealm
+//
+//  Created by Lera Savchenko on 11.09.23.
+//
+
+import Foundation
+
+class DataManager {
+    static let shared = DataManager()
+    
+    private init() {}
+    
+    func createTempData(completion: @escaping () -> Void) {
+        if !UserDefaults.standard.bool(forKey: "done") {
+            let shoppingList = TaskList()
+            shoppingList.name = "Shopping List"
+            
+            let milk = Task()
+            milk.name = "Milk"
+            milk.note = "1l"
+            
+            let bread = Task(value: ["Bread", "", Date(), true])
+            let apples = Task(value: ["name": "Apples", "note": "2kg"])
+            
+            shoppingList.tasks.append(milk)
+            shoppingList.tasks.insert(contentsOf: [bread, apples], at: 1)
+        }
+    }
+}
