42dcb4091d5518b488b0bb642d52b9452c4605a4
Lera Savchenko <58272000+Leralubiteklery@users.noreply.github.com>
2023-09-11 18:11:30 +0300

    Imported realm. Updated model.

--- TaskListsRealm/Models/TaskList.swift
+++ TaskListsRealm/Models/TaskList.swift
@@ -6,16 +6,17 @@
 //
 
 import Foundation
+import RealmSwift
 
-class TaskList {
-    var name = ""
-    var date = Date()
-    var tasks: [Task] = []
+class TaskList: Object {
+    @Persisted var name = ""
+    @Persisted var date = Date()
+    @Persisted var tasks = List<Task>()
 }
 
-class Task {
-    var name = ""
-    var note = ""
-    var date = Date()
-    var isCompleted = false
+class Task: Object {
+    @Persisted  var name = ""
+    @Persisted  var note = ""
+    @Persisted  var date = Date()
+    @Persisted  var isCompleted = false
 }
